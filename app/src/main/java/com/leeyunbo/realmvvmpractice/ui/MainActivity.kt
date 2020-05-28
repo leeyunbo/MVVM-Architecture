cddd3301cfe13f15c01c7078a4d65403b528883f
leeyunbo <servers1@naver.com>
2020-05-28 22:51:14 +0900

    [개발] View, ViewModel 분리

--- app/src/main/java/com/leeyunbo/realmvvmpractice/ui/MainActivity.kt
+++ app/src/main/java/com/leeyunbo/realmvvmpractice/ui/MainActivity.kt
@@ -3,20 +3,12 @@ package com.leeyunbo.realmvvmpractice.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.gson.annotations.SerializedName
 import com.leeyunbo.realmvvmpractice.R
-import com.leeyunbo.realmvvmpractice.data.UserVO
-import com.leeyunbo.realmvvmpractice.util.RetrofitFactory
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
+import com.leeyunbo.realmvvmpractice.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-    val service =
-        RetrofitFactory.getRetrofit().create(RetrofitNetwork::class.java)
+    private val viewModel =
+        MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,45 +17,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
-        service.getUserList().enqueue(object : Callback<List<UserVO>> {
-            override fun onFailure(call: Call<List<UserVO>>, t: Throwable) {
-                Log.e("IOException","===>getUserList() 요청 실패 서버 확인 바람")
-                println(t.printStackTrace())
-                call.cancel()
-            }
-
-            override fun onResponse(call: Call<List<UserVO>>, response: Response<List<UserVO>>) {
-                for(data in arrayOf(response.body())) {
-                    println(data?.toString())
-                }
-            }
-        })
-
-
-        service.getUser(1).enqueue(object : Callback<UserVO> {
-            override fun onFailure(call: Call<UserVO>, t: Throwable) {
-                Log.e("IOException","===>getUserList() 요청 실패 서버 확인 바람")
-                println(t.printStackTrace())
-                call.cancel()
-            }
-
-            override fun onResponse(call: Call<UserVO>, response: Response<UserVO>) {
-                println(response?.body().toString())
-            }
-        })
-
+        viewModel.onResume()
     }
-}
 
-interface RetrofitNetwork {
-    @GET("users")
-    fun getUserList() : Call<List<UserVO>>
-
-    @GET("/users/{id}")
-    fun getUser(@Path("id") id : Int) : Call<UserVO>
-
-    @POST("/users")
-    fun insertUser(@Body vo : UserVO) : Call<UserVO>
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i("onSaveInstanceState","=====> onSaveInstanceState() 실행")
+    }
 }
--- app/src/main/java/com/leeyunbo/realmvvmpractice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/leeyunbo/realmvvmpractice/viewmodel/MainViewModel.kt
@@ -1,2 +1,44 @@
 package com.leeyunbo.realmvvmpractice.viewmodel
 
+import android.util.Log
+import com.leeyunbo.realmvvmpractice.api.Api
+import com.leeyunbo.realmvvmpractice.data.UserVO
+import com.leeyunbo.realmvvmpractice.util.RetrofitFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel {
+    private val service =
+        RetrofitFactory.getRetrofit().create(Api::class.java)
+
+    fun onResume() {
+        service.getUserList().enqueue(object : Callback<List<UserVO>> {
+            override fun onFailure(call: Call<List<UserVO>>, t: Throwable) {
+                Log.e("IOException","===>getUserList() 요청 실패 서버 확인 바람")
+                println(t.printStackTrace())
+                call.cancel()
+            }
+
+            override fun onResponse(call: Call<List<UserVO>>, response: Response<List<UserVO>>) {
+                for(data in arrayOf(response.body())) {
+                    println(data?.toString())
+                }
+            }
+        })
+
+
+        service.getUser(1).enqueue(object : Callback<UserVO> {
+            override fun onFailure(call: Call<UserVO>, t: Throwable) {
+                Log.e("IOException","===>getUserList() 요청 실패 서버 확인 바람")
+                println(t.printStackTrace())
+                call.cancel()
+            }
+
+            override fun onResponse(call: Call<UserVO>, response: Response<UserVO>) {
+                println(response?.body().toString())
+            }
+        })
+
+    }
+}
